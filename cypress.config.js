3e957cea387816833d9f0da393bbe30c84017d12
Lizarb Venturim <lizarbpacheco@gmail.com>
2024-11-03 09:53:41 -0300

    feat: configure parameters

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { configureAllureAdapterPlugins } = require("@mmisty/cypress-allure-adapte
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://countries.trevorblades.com/",
+    failOnStatusCode: false,
+    specPattern: "**/*.cy.js",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       configureAllureAdapterPlugins(on, config);
 
