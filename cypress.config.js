219c3e167e75bab39d5e454a9e51e2af1a6d4d32
Lizarb Venturim <lizarbpacheco@gmail.com>
2024-11-02 21:50:38 -0300

    feat: configure allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
+const { configureAllureAdapterPlugins } = require("@mmisty/cypress-allure-adapter/plugins")
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      configureAllureAdapterPlugins(on, config);
+
+      return config;
     },
   },
 });
